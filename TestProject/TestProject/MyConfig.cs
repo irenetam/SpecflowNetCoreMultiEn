3f9cc11bd2069bf9f72794f3e3cb01e7014d4c0f
tam phan <tam.phan@altsourcesoftware.com>
2021-05-15 11:10:02 +0700

    change

---
+++ TestProject/TestProject/MyConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Altsource
+{
+    public class MyConfig
+    {
+        public string ApplicationName { get; set; }
+        public string Version { get; set; }
+        public string Headless { get; set; }
+        public string recipientsEmail { get; set; }
+    }
+}
